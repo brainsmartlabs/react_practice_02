f8b13c2f5a233fb0c8d04e1ad32d692136e708b3
Balaji The Boss <brainsmartlabs@gmail.com>
2023-01-20 02:07:07 +0530

    basic structure of news project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Component } from 'react';
 import './App.css';
+import Navbar from './navbar/Navbar';
+import News from './news/News';
+import NewsItem from './newsItem/NewsItem';
 
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+class App extends Component {
+
+  async componentDidMount() {
+    let res = await fetch('https://newsapi.org/v2/top-headlines?apiKey=202c614376af4b86b49c2823e2631480');
+    let data = await res.json();
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <News />
+      </>
+    )
+  }
 }
 
 export default App;
